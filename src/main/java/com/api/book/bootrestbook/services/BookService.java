51f894733b79ab22e2779570976808eca77956f7
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2024-04-11 20:57:07 -0400

    Revert "Connecting database MYSQL using JPA"
    This reverts commit 87634c6f3c65eb660ce2204ac0f7127acda10f11.
    
    "reverting to previous commit"

--- src/main/java/com/api/book/bootrestbook/services/BookService.java
+++ src/main/java/com/api/book/bootrestbook/services/BookService.java
@@ -2,32 +2,27 @@ package com.api.book.bootrestbook.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.api.book.bootrestbook.Entities.Book;
-import com.api.book.bootrestbook.dao.BookRepo;
 
 @Component
 public class BookService {
 
-    @Autowired
-    private BookRepo bookRepo;
-    // private static List<Book> list = new ArrayList<>();
+    private static List<Book> list = new ArrayList<>();
 
-    // static {
-    // list.add(new Book(1001, "Create your API", "Dev-DXS"));
-    // list.add(new Book(1002, "Configuring APIs", "Dev-DXS"));
-    // list.add(new Book(1003, "Depth Understaing Of Application Architecture",
-    // "Dev-DXS"));
-    // list.add(new Book(1004, "World Of Devlopment", "DXS_Edu"));
-    // }
+    static {
+        list.add(new Book(1001, "Create your API", "Dev-DXS"));
+        list.add(new Book(1002, "Configuring APIs", "Dev-DXS"));
+        list.add(new Book(1003, "Depth Understaing Of Application Architecture",
+                "Dev-DXS"));
+        list.add(new Book(1004, "World Of Devlopment", "DXS_Edu"));
+    }
 
     // method to return list of all the books
     public List<Book> getAllBooks() {
-
-        List<Book> list = (List<Book>) this.bookRepo.findAll();
         return list;
     }
 
@@ -37,7 +32,7 @@ public class BookService {
         // Using stream
         Book b2 = null;
         try {
-            b2 = this.bookRepo.findById(bookID);
+            b2 = list.stream().filter(e -> e.getBookID() == bookID).findFirst().get();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -57,26 +52,26 @@ public class BookService {
     // Adding a book
     public Book addBook(Book b) {
 
-        Book newBook = this.bookRepo.save(b);
-        return newBook;
+        list.add(b);
+        return b;
     }
 
     // Deleting a book
     public List<Book> deleteBookByID(int bookID) {
 
-        this.bookRepo.deleteById(bookID);
-        List<Book> list = getAllBooks();
+        Book b1 = getBookByID(bookID);
+        list.remove(b1);
+        // list = list.stream().filter(e -> e.getBookID() !=
+        // bookID).collect(Collectors.toList());
         return list;
     }
 
     // Updating a book
     public void updateBookById(Book book, int bookID) {
 
-        book.setBookID(bookID);
-        bookRepo.save(book);
-        // Book b = getBookByID(bookID);
-        // b.setTitle(book.getTitle());
-        // b.setAuthor(book.getAuthor());
+        Book b = getBookByID(bookID);
+        b.setTitle(book.getTitle());
+        b.setAuthor(book.getAuthor());
         // list = list.stream().map(e -> {
         // if (e.getBookID() == bookID) {
         // e.setTitle(book.getTitle());
